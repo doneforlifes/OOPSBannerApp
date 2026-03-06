bf2a06b2c874bae1fd7d786ec9c7547a80cdb793
doneforlifes <nithwin.shibil@gmail.com>
2026-03-06 11:02:02 +0530

    UC2 implemented - print banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ * UC2: Print OOPS as Banner
+ * @author Kailas
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******   ***** ");
+        System.out.println("*     *  *     *   *     *  *     ");
+        System.out.println("*     *  *     *   *     *  *     ");
+        System.out.println("*     *  *     *   ******    *****");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println(" *****    *****    *        ***** ");
+
+    }
+}
